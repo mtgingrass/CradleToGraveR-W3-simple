fc0c430226edc08047603d8ddb1e4cd940d22c38
mtgingrass <mark.gingrass@gmail.com>
2020-06-19 22:29:03 -0400

    Testing iterating through file list.

--- YouTube_to_RMD_File.R
+++ YouTube_to_RMD_File.R
@@ -24,66 +24,76 @@ yt_dl_descrip_dir <- list.files(path = "c:/ytdl/",
 L1 <- yt_dl_JSON
 L2 <- yt_dl_vtt_cnvrt
 
-inds <- match(sub('\\..*', '', basename(L1)), sub('\\..*', '', basename(L2)))
-inds2 <- match(sub('\\..*', '', basename(L1)), sub('\\..*', '', basename(content_files)))
-df_yt <- data.frame(JSON = L1, VTT = L2[inds], IMAGE = content_files[inds2])
+inds <- match(sub('\\..*', '', basename(yt_dl_JSON)), 
+              sub('\\..*', '', basename(yt_dl_vtt_cnvrt)))
 
-#read first json file
-yt_json_file <- read_json(df_yt$JSON[1])
+inds2 <- match(sub('\\..*', '', basename(yt_dl_JSON)), 
+               sub('\\..*', '', basename(content_files)))
 
-yt_title <- yt_json_file[1]$title
+df_yt <- data.frame(JSON = yt_dl_JSON, VTT = yt_dl_vtt_cnvrt[inds],
+                    IMAGE = content_files[inds2])
 
-#yt_descrip <- readtext(yt_dl_descrip_dir[1])
-yt_descrip <- yt_json_file$description
+### I could use the DF itself instead.
+### Iterate through each json file. 
 
-title <- paste("---\ntitle: ", yt_json_file$title, sep = "")
-
-date_upload <- paste("date: ", 
-                     substr(yt_json_file$upload_date,1,4),
-                     "-",
-                     substr(yt_json_file$upload_date,5,6),
-                     "-",
-                     substr(yt_json_file$upload_date,7,8),
-                     sep = "")
-
-play_list_cat <- paste("categories:",
-                  "\n",
-                  "  - ",
-                  yt_json_file$playlist_title,
-                  sep = "")
-
-
-image_path <- df_yt[1, 3]
-
-yt_tags <- paste("tags:",
-                       "Test","Test2",
-                       sep = "\n  - ")
-
-w3yaml <- "type:  \"post\"
-w3codecolor: false
-draft: false"
-
-descript <- paste("**","TEST 1","**\n\n",
-                  yt_descrip, sep = "\n")
-
-# Combined YAML
-yaml_tmp <- paste(title,
-                  date_upload,
-                  play_list_cat,
-                  yt_tags,
-                  "",
-                  w3yaml,
-                  "---",
-                  "",
-                  paste("<img src=", '"https://www.cradletograver.com/auto-posts/images/',
-                        image_path,'"', ">", sep = ""),
-                  yt_json_file$description,
-                  sep = "\n")
-
-
-
-write(yaml_tmp,
-      file = paste("C:\\Users\\markg\\Documents\\CradleToGraveR-W3-simple2\\content\\english\\auto-posts\\",
-                   str_replace_all(yt_json_file$title, "[^[:alnum:]]", "-"), ".Rmd", sep=""), 
-                   append = FALSE)
-
+for (index in df_yt)
+{
+  yt_json_file <- read_json(df_yt$JSON[index])
+  
+  yt_title <- yt_json_file[index]$title
+  
+  #yt_descrip <- readtext(yt_dl_descrip_dir[index])
+  yt_descrip <- yt_json_file$description
+  
+  title <- paste("---\ntitle: ", yt_json_file$title, sep = "")
+  
+  date_upload <- paste("date: ", 
+                       substr(yt_json_file$upload_date,1,4),
+                       "-",
+                       substr(yt_json_file$upload_date,5,6),
+                       "-",
+                       substr(yt_json_file$upload_date,7,8),
+                       sep = "")
+  
+  play_list_cat <- paste("categories:",
+                    "\n",
+                    "  - ",
+                    yt_json_file$playlist_title,
+                    sep = "")
+  
+  
+  image_path <- df_yt[index, 3]
+  
+  yt_tags <- paste("tags:",
+                         "Test","Test2",
+                         sep = "\n  - ")
+  
+  w3yaml <- "type:  \"post\"
+  w3codecolor: false
+  draft: false"
+  
+  descript <- paste("**","TEST 1","**\n\n",
+                    yt_descrip, sep = "\n")
+  
+  # Combined YAML
+  yaml_tmp <- paste(title,
+                    date_upload,
+                    play_list_cat,
+                    yt_tags,
+                    "",
+                    w3yaml,
+                    "---",
+                    "",
+                    paste("<img src=", '"https://www.cradletograver.com/auto-posts/images/',
+                          image_path,'"', ">", sep = ""),
+                    yt_json_file$description,
+                    sep = "\n")
+  
+  
+  
+  write(yaml_tmp,
+        file = paste("C:\\Users\\markg\\Documents\\CradleToGraveR-W3-simple2\\content\\english\\auto-posts\\",
+                     str_replace_all(yt_json_file$title, "[^[:alnum:]]", "-"), ".Rmd", sep=""), 
+                     append = FALSE)
+  
+}
